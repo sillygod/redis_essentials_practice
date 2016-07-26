8ba32b2dc2a95e8b4f981f9c47ba4d070ed3d720
Hanzawa Ye <sillygod@livemail.tw>
2016-07-26 18:08:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# the study for Redis Essentials
+
+I learn and practice redis with book's example codes in python and keep notes in this repo.
---
+++ ch1/articles-popularity.py
@@ -0,0 +1,44 @@
+import redis
+
+
+class RedisClient:
+
+    """
+    a simple wrapper for redis function
+    """
+
+    def __init__(self):
+        self.client = redis.StrictRedis(host='localhost', port=6379)
+
+    def upVote(self, id):
+        key = "article:{}:votes".format(id)
+        self.client.incr(key)
+
+    def downVote(self, id):
+        key = "article:{}:votes".format(id)
+        self.client.decr(key)
+
+    def showResults(self, id):
+        headlineKey = "article:" + str(id) + ":headline"
+        voteKey = "article:" + str(id) + ":votes"
+
+        res = self.client.mget([headlineKey, voteKey])
+        print("The article {} has {} votes".format(res[0], res[1]))
+
+
+if __name__ == "__main__":
+    client = RedisClient()
+
+    client.upVote(12345)
+    client.upVote(12345)
+    client.upVote(12345)
+    client.upVote(10001)
+    client.upVote(10001)
+    client.downVote(10001)
+    client.upVote(60056)
+
+    client.showResults(12345)
+    client.showResults(10001)
+    client.showResults(60056)
+
+
