e5fe2ebe010c05471a5dccec9a916bb002bb7e76
sillygod <sillygod@livemail.tw>
2016-12-20 12:14:12 +0800

    work on ch2

--- ch2/deal_metrics.py
+++ ch2/deal_metrics.py
@@ -2,6 +2,9 @@
 #  - mark a deal as sent to a user
 #  - check whether a user received a group of deals
 #  - gather metrics from the sent deals
+import redis
+import logging
+
 
 class RedisClient:
 
@@ -15,4 +18,20 @@ class RedisClient:
     def markDealAsSent(self, dealId, userId):
         # add user to deal set
         self._client.sadd(dealId, userId)
+
+    def sendDealIfNotSent(self, dealId, userId):
+        ismember = self._client.sismember(dealId, userId)
+        if ismember:
+            logging.info('Deal {} was already sent to user {}'.format(dealId, userId))
+        else:
+            logging.info('Sending {} to user {}'.format(dealId, userId))
+            self.markDealAsSent(dealId, userId)
+
+    def showUsersThatReceivedAllDeals(self, dealIds):
+        res = self._client.sinter(dealIds)
+        logging.info('{} received all of dealIds: {}'.format([x for x in res], dealIds))
+
+    def showUsersThatReceiveAtLeastOneOfTheDeals(self, dealIds):
+        pass
+
 
